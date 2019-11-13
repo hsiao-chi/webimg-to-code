19220758b829e87b3e774cd3f8e38adf1a6f3ae3
chi <ciyalnin@gmail.com>
2019-11-13 18:38:47 +0800

    set LAB path Config

--- general/path.py
+++ general/path.py
@@ -1,40 +1,41 @@
 # class
+SELF = 'D:\\Chi\\webimg-to-code\\'
+CLASS_SEQ2SEQ_MODEL_GRAPH_FILE = SELF+'assets\\seq2seq-position\\seq2seq-model.png'
 
-CLASS_SEQ2SEQ_MODEL_GRAPH_FILE = 'E:\\projects\\NTUST\\webimg-to-code\\assets\\seq2seq-position\\seq2seq-model.png'
+CLASS_SEQ2SEQ_WEIGHT = SELF+'assets\\seq2seq-position\\weight\\'
+CLASS_SEQ2SEQ_ANALYSIS_PATH = SELF+'assets\\seq2seq-position\\'
+CLASS_SEQ2SEQ_PREDIT_GUI_PATH = SELF+'assets\\seq2seq-position\\test-predit-gui\\'
 
-CLASS_SEQ2SEQ_WEIGHT = 'E:\\projects\\NTUST\\webimg-to-code\\assets\\seq2seq-position\\weight\\'
-CLASS_SEQ2SEQ_ANALYSIS_PATH = 'E:\\projects\\NTUST\\webimg-to-code\\assets\\seq2seq-position\\'
-CLASS_SEQ2SEQ_PREDIT_GUI_PATH = 'E:\\projects\\NTUST\\webimg-to-code\\assets\\seq2seq-position\\test-predit-gui\\'
-
-# PATH_PIX2CODE_DATASET = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\'
+# PATH_PIX2CODE_DATASET = SELF+'dataset\\pix2code\\'
 # PIX2CODE_POSITION_FOLDER = 'row-col-yolo-position-txt\\'
 # PIX2CODE_GUI_FOLDER = 'row-col-gui\\'
 
 
 # DATASET
-DATASET1_ASSEST = 'E:\\projects\\NTUST\\webimg-to-code\\transform_pix2code_dataset\\assest\\'
-DATASET1_ROWCOL_GUI = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\row-col-gui\\'
-DATASET1_ROWCOL_PNG = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\row-col-png\\'
-DATASET1_ROWCOL_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\row-col-position-txt\\'
-DATASET1_ROWCOL_YOLO_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\row-col-yolo-position-txt\\'
-DATASET1_YOLO_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\yolo-position-txt\\'
-DATASET1_FULL_YOLO_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\full-yolo-position-txt\\'
-DATASET1_ORIGIN_PNG = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\pix2code\\origin-png\\'
+DATASET = 'D:\\Chi\\dataset\\'
+DATASET1_ASSEST = SELF+'transform_pix2code_dataset\\assest\\'
+DATASET1_ROWCOL_GUI = DATASET+'pix2code\\row-col-gui\\'
+DATASET1_ROWCOL_PNG = DATASET+'pix2code\\row-col-png\\'
+DATASET1_ROWCOL_POSITION_TXT = DATASET+'pix2code\\row-col-position-txt\\'
+DATASET1_ROWCOL_YOLO_POSITION_TXT = DATASET+'pix2code\\row-col-yolo-position-txt\\'
+DATASET1_YOLO_POSITION_TXT = DATASET+'pix2code\\yolo-position-txt\\'
+DATASET1_FULL_YOLO_POSITION_TXT = DATASET+'pix2code\\full-yolo-position-txt\\'
+DATASET1_ORIGIN_PNG = DATASET+'pix2code\\origin-png\\'
 PIX2CODE_ORIGIN_DATASET = 'E:\\projects\\webGener\\pix2code\\datasets\\pix2code_datasets\\web\\all_data\\'
 
 
-DATASET2_ASSESTS = 'E:\\projects\\NTUST\\webimg-to-code\\datasetCode\\dataset_2_generator\\assests\\'
-DATASET2_ORIGIN_GUI = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\origin_gui\\'
-DATASET2_ORIGIN_HTML = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\origin_html\\'
-DATASET2_ORIGIN_PNG = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\origin_png\\'
-DATASET2_ROWCOL_GUI = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\row_col_gui\\'
-DATASET2_ROWCOL_IMG = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\row_col_png\\'
-DATASET2_ROWCOL_YOLO_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\row_col_yolo_position_txt\\'
-DATASET2_YOLO_POSITION_TXT = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\yolo_position_txt\\'
+DATASET2_ASSESTS = SELF+'datasetCode\\dataset_2_generator\\assests\\'
+DATASET2_ORIGIN_GUI = DATASET+'dataset2\\origin_gui\\'
+DATASET2_ORIGIN_HTML = DATASET+'dataset2\\origin_html\\'
+DATASET2_ORIGIN_PNG = DATASET+'dataset2\\origin_png\\'
+DATASET2_ROWCOL_GUI = DATASET+'dataset2\\row_col_gui\\'
+DATASET2_ROWCOL_IMG = DATASET+'dataset2\\row_col_png\\'
+DATASET2_ROWCOL_YOLO_POSITION_TXT = DATASET+'dataset2\\row_col_yolo_position_txt\\'
+DATASET2_YOLO_POSITION_TXT = DATASET+'dataset2\\yolo_position_txt\\'
 
-# DATASET2_ORIGIN_PNG_PATH = 'E:\\projects\\NTUST\\webimg-to-code\\dataset\\dataset2\\origin_png'
+# DATASET2_ORIGIN_PNG_PATH = DATASET+'dataset2\\origin_png'
 
-DATASET2_DSL_MAPPING_JSON_FILE = 'E:\\projects\\NTUST\\webimg-to-code\\datasetCode\\dataset_2_generator\\mapping\\web-dsl-mapping.json'
+DATASET2_DSL_MAPPING_JSON_FILE = SELF+'datasetCode\\dataset_2_generator\\mapping\\web-dsl-mapping.json'
 
 
-DATASETCODE_ASSESTS = 'E:\\projects\\NTUST\\webimg-to-code\\datasetCode\\assests\\'
+DATASETCODE_ASSESTS = SELF+'datasetCode\\assests\\'
