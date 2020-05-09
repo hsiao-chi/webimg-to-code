4131ac62b34c9fc6e58e3fb8ed00bd9cd9aba076
chi <ciyalnin@gmail.com>
2020-05-09 23:20:30 +0800

    fix evaluation_error

--- evaluationCode/evaluation_error.py
+++ evaluationCode/evaluation_error.py
@@ -14,9 +14,9 @@ class Eva_error:
         'error_history': []
         ,
         'final_error_score': {
-            'the_number_of_high_error(0.3)_data': 0,
-            'sum_error': 0,
-            'sum_len': 1,
+            'the_number_of_len_nonequal_data': 0,
+            # 'sum_error': 0,
+            # 'sum_len': 1,
             'error_score': 0
         }
     }
@@ -25,40 +25,44 @@ class Eva_error:
         self.record_template['config']['target_path']=target_path
         self.record_template['config']['model_path']=predit_model_path
         self.record_template['config']['start_index']=start_index
+        self.record_template['final_error_score']['the_number_of_len_nonequal_data']=0
         self.record_template['error_history']=[]
 
         self.each_error = np.zeros(1)
-        self.each_len = np.zeros(1)
+        # self.each_len = np.zeros(1)
 
     def cal_error(self, predited_sequence: list, original_sequence:list):
         error=0
-        max_seq_len = max(len(original_sequence), len(predited_sequence))
-        for idx in range(max_seq_len):
-           try:
-               if original_sequence[idx] != predited_sequence[idx]:
-                   error+=1
-           except IndexError:
-               error+=1
+        len_origin = len(original_sequence)
+        len_pred = len(predited_sequence)
+        if len_origin != len_pred:
+            error = 1
+            self.record_template['final_error_score']['the_number_of_len_nonequal_data']+=1
+
+        else:
+            error_s = 0
+            for idx in range(len_origin):
+                if predited_sequence[idx] != original_sequence[idx]:
+                    error_s+=1
+            
+            error = error_s/len_origin
+
         self.each_error = np.append(self.each_error, error)
-        self.each_len = np.append(self.each_len, max_seq_len)
-        if error/ max_seq_len > 0.3:
-            self.record_template['error_history'].append({
-                'idx': np.size(self.each_error)-2,
-                'error_len': [error, max_seq_len]
-            })
-        return error, max_seq_len
+        # self.each_len = np.append(self.each_len, max_seq_len)
+
+        return error
 
     def get_final_error(self, save_path: None):
         print('self.each_error', self.each_error)
-        print('self.each_len', self.each_len)
+        # print('self.each_len', self.each_len)
         sum_error = np.sum(self.each_error)
-        sum_len = np.sum(self.each_len)
-        error_score = sum_error/sum_len
-        self.record_template['config']['num_of_data']= np.size(self.each_error)-1
-        self.record_template['final_error_score']['sum_error']= sum_error
-        self.record_template['final_error_score']['sum_len']= sum_len
+        len_data = np.size(self.each_error)-1
+        error_score = sum_error/len_data
+        self.record_template['config']['num_of_data']= len_data
+        # self.record_template['final_error_score']['sum_error']= sum_error
+        # self.record_template['final_error_score']['sum_len']= sum_len
         self.record_template['final_error_score']['error_score']= error_score
-        self.record_template['final_error_score']['the_number_of_high_error(0.3)_data']= len(self.record_template['error_history'])
+        # self.record_template['final_error_score']['the_number_of_high_error(0.3)_data']= len(self.record_template['error_history'])
         
         if save_path:
             with open(save_path, 'a+') as file:
