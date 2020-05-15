0d5c6fad9109f753706f72fe9295ed068bcfb5c2
chi <ciyalnin@gmail.com>
2020-05-15 22:39:44 +0800

    model/attention

---
+++ classes/model/seq2seq_biLSTM_attention.py
@@ -0,0 +1,151 @@
+from keras.models import Model, Sequential
+from keras.layers import Input, LSTM, Dense, GaussianNoise, Bidirectional, concatenate, Concatenate, Activation, dot, TimeDistributed, Embedding
+from keras import backend as K, callbacks
+import numpy as np
+from classes.data2Input import to_Seq2Seq_encoder_input, encoder_tokens_list_to_dict
+from general.util import createFolder, write_file, showLoss, showAccuracy
+import general.dataType as TYPE
+
+ENCODER_LSTM_VEC = 64
+DECODER_LSTM_VEC = 64
+WORD_EMBD_VEC = 64
+SEQ2SEQ_EPOCHES = 10
+MODE_SAVE_PERIOD = 100
+BATCH_SIZE=64
+
+def lstm_attention_model(num_encoder_input_vec: int, max_decoder_output_length=300, output_dict_size=19)->Model:
+    encoder_input = Input(shape=(None, num_encoder_input_vec), name="encoder_input")
+    decoder_input = Input(shape=(max_decoder_output_length,), name="decoder_input")
+    encoder = GaussianNoise(1, name="gaussian_noise")(encoder_input)
+    encoder = LSTM(ENCODER_LSTM_VEC, return_sequences=True, name="encoder_lstm")(encoder_input)
+    encoder_last = encoder[:,-1,:]
+    print('encoder', encoder)
+    print('encoder_last', encoder_last)
+    decoder = Embedding(output_dict_size, DECODER_LSTM_VEC, input_length=max_decoder_output_length, mask_zero=True, name="decoder_embedding")(decoder_input)
+    decoder = LSTM(DECODER_LSTM_VEC, return_sequences=True, name="decoder_lstm")(decoder, initial_state=[encoder_last, encoder_last])
+
+    attention = dot([decoder, encoder], axes=[2, 2], name="dot1")
+    attention = Activation('softmax', name='attention')(attention)
+    print('attention', attention)
+
+    context = dot([attention, encoder], axes=[2,1], name="dot2")
+    print('context', context)
+
+    decoder_combined_context = Concatenate(name="decoder_combined_context")([context, decoder])
+    print('decoder_combined_context', decoder_combined_context)
+
+    # Has another weight + tanh layer as described in equation (5) of the paper
+    output = TimeDistributed(Dense(64, activation="tanh"), name="dense1")(decoder_combined_context)
+    output = TimeDistributed(Dense(output_dict_size, activation="softmax"), name="dense2")(output)
+    print('output', output)
+    model = Model(inputs=[encoder_input, decoder_input], outputs=[output])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+def mapping_gui_skeleton_token_index(seqs, token_list: list, max_len):
+    temp=[]
+    _max_len = max_len
+    eos = token_list.index('EOS')
+    for seq in seqs:
+        t = [token_list.index(t) for t in seq]
+        t = [token_list.index('START')]+t+[eos]
+        _max_len = max(_max_len, len(t))
+        temp.append(t)
+    target = np.zeros((len(temp), _max_len))
+    for idx in range(len(temp)):
+        for j in range(_max_len):
+            try:
+                target[idx, j] = temp[idx][j]
+            except IndexError:
+                target[idx, j] = eos
+    return target
+
+def to_batch_encoder_input(input_seqs_list: list, encoder_config, max_len=50) -> np.array:
+    encoder_tokens = encoder_tokens_list_to_dict(encoder_config['token_list'], encoder_config['class_mode'])
+    _max_len=max_len
+    full_data=[]
+    for input_seqs in input_seqs_list:
+        temp_data = []
+        _max_len = max(_max_len, len(input_seqs))
+        for input_seq in input_seqs:
+            data = input_seq[:encoder_config['direct_part']]
+            attrs = [0]*len(encoder_tokens)
+            for attr in input_seq[encoder_config['direct_part']:]:
+                if encoder_config['class_mode']:
+                    for idx, target_list in enumerate(encoder_tokens):
+                        try:
+                            attrs[idx] = target_list[attr]
+                        except KeyError:
+                            pass
+                else:
+                    attrs[encoder_tokens[attr]] = 1
+            temp_data.append(data+attrs)
+        full_data.append(temp_data)
+
+    encoder_input_data = np.zeros((len(input_seqs_list), _max_len, len(encoder_tokens)+encoder_config['direct_part']), dtype='float32')
+    for d, data in enumerate(full_data):
+        for i, line in enumerate(data):
+            encoder_input_data[d, i] = line
+    return encoder_input_data
+
+
+def lstm_attention_training(model: Model, encoder_input_list: list, decoder_input_list: list, 
+encoder_config, decoder_config, checkpoint_folder, analysis_saved_folder, final_model_saved_path, encoder_max_len=50, decoder_max_len=250):
+    '''
+    @ param encoder_input_list: list, 3dim,(batch_size, input_length, 5~7), attribute還是attribute 座標還是座標
+
+    @ param decoder_input_list: 2dim, (batch_size, gui_skeleton_list)不含'START', 'EOS'
+    
+    @ param attributes_dict: [attributes]
+
+    '''
+    training_encoder_input=to_batch_encoder_input(encoder_input_list, encoder_config, encoder_max_len)
+    # print('training_encoder_input: ', training_encoder_input.shape, '\n', training_encoder_input)
+
+    training_decoder_input = mapping_gui_skeleton_token_index(decoder_input_list, decoder_config['token_list'], decoder_max_len)
+    _training_decoder_output = np.roll(training_decoder_input, -1, axis=1)
+    _training_decoder_output[:, -1] = _training_decoder_output[:, -2]
+    training_decoder_output=np.eye(len(decoder_config['token_list']))[_training_decoder_output.astype('int')]
+
+    # print('training_decoder_input: ', training_decoder_input.shape, '\n', training_decoder_input)
+    # print('training_decoder_output: ', training_decoder_output.shape, '\n', training_decoder_output)
+
+    mc = callbacks.ModelCheckpoint(checkpoint_folder + str(SEQ2SEQ_EPOCHES) + '\\seq2seq-weights{epoch:05d}.h5',
+                                   save_weights_only=True, period=MODE_SAVE_PERIOD)
+    history = model.fit([training_encoder_input, training_decoder_input], training_decoder_output,
+                              batch_size=BATCH_SIZE,
+                              epochs=SEQ2SEQ_EPOCHES,
+                              validation_split=0.2,
+                              initial_epoch=0,
+                              callbacks=[mc])
+    showLoss(history, analysis_saved_folder, 'loss' + str(SEQ2SEQ_EPOCHES))
+    showAccuracy(history, analysis_saved_folder,
+                 'accuracy' + str(SEQ2SEQ_EPOCHES))
+    write_file(history.history, analysis_saved_folder +
+               'history'+str(SEQ2SEQ_EPOCHES)+TYPE.TXT, 'JSON')
+    model.save(final_model_saved_path)
+    return model
+
+
+def generate(model: Model, encoder_input_list, encoder_config, max_output_len, gui_token_dict: list):
+    encoder_input = to_Seq2Seq_encoder_input(encoder_input_list, encoder_config)
+    decoder_input = np.zeros(shape=(len(encoder_input), max_output_len))
+    decoder_input[:,0] = gui_token_dict.index('START')
+    eos = gui_token_dict.index('EOS')
+    for i in range(max_output_len):
+        output = model.predict([encoder_input, decoder_input]).argmax(axis=2)
+        decoder_input[:,i] = output[:,i]
+        if output[:,i] == eos:
+            break
+    decoder_output=[]
+    print('decoder_input', decoder_input)
+    for i in range(max_output_len):
+        if decoder_input[0][i] != eos:
+            decoder_output.append(gui_token_dict[decoder_input[0][i].astype('int')])
+        else:
+            break
+    return decoder_output
+
+# def evaluation()
---
+++ seq2seq.py
@@ -0,0 +1,168 @@
+from classes.get_configs import get_encoder_config, get_decoder_config
+from classes.data2Input import to_Seq2Seq_input, to_Seq2Seq_encoder_input, decoder_tokens_list_to_dict
+from classes.model.seq2seq_biLSTM_attention import lstm_attention_model, mapping_gui_skeleton_token_index, lstm_attention_training, generate, SEQ2SEQ_EPOCHES
+import os
+import os.path
+import general.dataType as TYPE
+import general.path as path
+from general.util import createFolder, showLoss, showAccuracy, read_file, write_file
+from keras.models import load_model
+import random
+from evaluationCode.bleu import Bleu
+from evaluationCode.evaluation_error import Eva_error
+
+def positions_to_encoder_input(data_folder, data_num, start_index):
+    encoder_input = []
+    max_encoder_input_len = 0
+    for idx in range(start_index, start_index+data_num):
+        temp_lines = read_file(data_folder+str(idx)+TYPE.TXT, 'splitlines')
+        max_encoder_input_len = max(max_encoder_input_len, len(temp_lines))
+        temp = [temp_line.split() for temp_line in temp_lines]
+        encoder_input.append(temp)
+    return encoder_input, max_encoder_input_len
+
+def gui_to_decoder_input(data_folder, data_num, start_index):
+    decoder_input = []
+    max_decoder_input_len = 0
+    for idx in range(start_index, start_index+data_num):
+        temp = read_file(data_folder+str(idx)+TYPE.GUI,'splitBySpec')
+        max_decoder_input_len = max(max_decoder_input_len, len(temp))
+        decoder_input.append(temp)
+    return decoder_input, max_decoder_input_len
+
+if __name__ == "__main__":
+    INPUT_TYPE = 1
+    TARGET_TYPE = 3
+    # seq_model_type = SeqModelType.encoder_bidirectional_attention.value
+    num_encoder_input_vec=5 #5 or 15
+    max_encoder_len=50
+    max_decoder_len=250
+    layer2_lstm = False
+    training_data_num = 500
+    training_start_idx = 0
+    evaluate_data_num = 500
+    eva_record_file_path = path.EVALUATION_SEQ2SEQ_EVALUATION+'pix2code\\'
+    eva_record_file_name = 'Arch1_test.txt'
+    predit_data_nums = [1, 1] # train, test
+    # predit_test_data = False
+    
+    bleu_record_file_path =  path.EVALUATION_BLEU_SCORE + 'layout_generate_only\\pix2code\\'
+    bleu_record_file_name = 'Arch1_test.txt'
+
+    error_record_file_path =  path.EVALUATION_ERROR_SCORE + 'layout_generate_only\\pix2code\\'
+    error_record_file_name = 'Arch1_test_bn1.txt'
+    
+    gaussian_noise = None  # None
+    early_stoping = False
+    TRAINING = False
+    PREDIT = True
+    EVALUATE = False
+    BLEU_SCORE = False
+    ERROR_SCORE = False
+
+    encoder_config = get_encoder_config(INPUT_TYPE)
+    decoder_config = get_decoder_config(TARGET_TYPE)
+    final_model_path = path.CLASS_SEQ2SEQ_MODEL_PATH + str(SEQ2SEQ_EPOCHES)+'\\model'+TYPE.H5
+    predit_model_path = final_model_path
+    # evaluate_model_path = r'E:\projects\NTUST\webimg-to-code\assets\2020\seq2seq-pix2code\full-rowcolAttrElement\2500\bidirectional-resort-noise\model\300\model.h5'
+    evaluate_model_path = final_model_path
+
+    if TRAINING:
+        createFolder(path.CLASS_SEQ2SEQ_MODEL_PATH + str(SEQ2SEQ_EPOCHES))
+        createFolder(path.CLASS_SEQ2SEQ_WEIGHT + str(SEQ2SEQ_EPOCHES))
+        encoder_train_input, max_encoder_input_len  = positions_to_encoder_input(encoder_config['data_folder'], training_data_num, training_start_idx)
+        decoder_train_input, max_decoder_input_len = gui_to_decoder_input(decoder_config['data_folder'], training_data_num, training_start_idx)
+        # print('main encoder_train_input', max_encoder_input_len, encoder_train_input)
+        # print('main decoder_train_input', max_decoder_input_len, decoder_train_input)
+
+
+        seq2seq_training_model = lstm_attention_model(num_encoder_input_vec=num_encoder_input_vec, 
+                max_decoder_output_length=max_decoder_len, 
+                output_dict_size=len(decoder_config['token_list']))
+        seq2seq_training_model = lstm_attention_training(seq2seq_training_model, 
+                encoder_input_list=encoder_train_input,
+                decoder_input_list=decoder_train_input,
+                encoder_config=encoder_config,
+                decoder_config=decoder_config,
+                checkpoint_folder=path.CLASS_SEQ2SEQ_WEIGHT +str(SEQ2SEQ_EPOCHES)+"\\",
+                analysis_saved_folder=path.CLASS_SEQ2SEQ_ANALYSIS_PATH,
+                final_model_saved_path=final_model_path,
+                encoder_max_len=max_encoder_len,
+                decoder_max_len=max_decoder_len)
+
+    # if EVALUATE:
+        # createFolder(eva_record_file_path)
+        # evaluate_save_text=''
+        # str_model_path = 'evaluated Model path: \n{}'.format(evaluate_model_path)
+        # str_training = '\ntraining data path: \n encoder: {}\n decoder: {}'.format(encoder_config['data_folder'], decoder_config['data_folder'])
+        # print(str_model_path)
+        # print(str_training)
+        # encoder_input_data, decoder_input_data, decoder_target_tokens, max_decoder_len = to_Seq2Seq_input(
+        #     encoder_config['data_folder'], decoder_config['data_folder'], encoder_config, decoder_config['token_list'], data_num=evaluate_data_num)
+
+        # str_training_result = seq2seq_evaluate(load_model(evaluate_model_path), encoder_input_data,
+        #                  decoder_input_data, decoder_target_tokens)
+        # str_testing = '\n\ntesting data path: \n encoder: {}\n decoder: {}'.format(
+        #     encoder_config['testing_data_folder'], decoder_config['testing_data_folder'])
+        # print(str_testing)
+        # encoder_input_data, decoder_input_data, decoder_target_tokens, max_decoder_len = to_Seq2Seq_input(
+        #     encoder_config['testing_data_folder'], decoder_config['testing_data_folder'], encoder_config, decoder_config['token_list'])
+
+        # str_testing_result = seq2seq_evaluate(load_model(evaluate_model_path), encoder_input_data,
+        #                  decoder_input_data, decoder_target_tokens)
+        # evaluate_save_text = str_model_path+str_training+str_training_result+str_testing+str_testing_result
+        # write_file(evaluate_save_text, eva_record_file_path+eva_record_file_name, dataDim=0)
+        
+    
+    if PREDIT:
+        createFolder(path.CLASS_SEQ2SEQ_PREDIT_GUI_PATH + str(SEQ2SEQ_EPOCHES))
+
+        model = load_model(predit_model_path)
+        for data_folder, predit_data_num in zip(['data_folder', 'testing_data_folder'], predit_data_nums):
+            valid_data_num = predit_data_num
+
+            if BLEU_SCORE:
+                bleu = Bleu(predit_data_num, 0, encoder_config[data_folder], decoder_config[data_folder], predit_model_path)
+            if ERROR_SCORE:
+                eva_error = Eva_error(0, encoder_config[data_folder], decoder_config[data_folder], predit_model_path)
+            for i in range(predit_data_num):
+                reference_gui = None
+                input_seqs = read_file(
+                    encoder_config[data_folder]+str(i)+TYPE.TXT, 'splitlines')
+                if len(input_seqs)==0:
+                    valid_data_num -= 1
+                    continue
+                
+                input_seqs = [seq.split() for seq in input_seqs]
+                decoded_sentence = generate(model, 
+                encoder_input_list=input_seqs,
+                encoder_config=encoder_config,
+                max_output_len=max_decoder_len,
+                gui_token_dict=decoder_config['token_list'])
+                
+                # print('decoded_sentence length: ', i, len(decoded_sentence)) if i%50==0 and BLEU_SCORE else None
+                print('decoded_sentence length: ', i, decoded_sentence) 
+
+                if BLEU_SCORE:
+                    reference_gui = read_file(
+                        decoder_config[data_folder]+str(i)+TYPE.GUI, 'splitBySpec')
+                    bleu.evaluate(decoded_sentence, reference_gui)
+                if ERROR_SCORE:
+                    reference_gui = reference_gui if reference_gui else read_file(
+                        decoder_config[data_folder]+str(i)+TYPE.GUI, 'splitBySpec')
+                    e= eva_error.cal_error(decoded_sentence, reference_gui)
+                    print(e)
+            if BLEU_SCORE:
+                createFolder(bleu_record_file_path)
+                p = bleu_record_file_path+bleu_record_file_name
+                print(p)
+                bleu.save_evaluation(p, valid_data_num)
+            
+            if ERROR_SCORE:
+                createFolder(error_record_file_path)
+                p = error_record_file_path+error_record_file_name
+                print(p)
+                eva_error.get_final_error(p)
+
+
+    
